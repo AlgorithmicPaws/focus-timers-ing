50f882b6e111b5e44ee8fc3e714e1001cf5cbe66
Daniel Mora <danielleonardomora.04@gmail.com>
2024-11-24 22:13:22 -0500

    detalles de css, desplegable e imagenes

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,30 +1,42 @@
 import './header.css'
 import logo from '../../assets/images/logo.svg';
+import user from '../../assets/images/user.svg';
+import settings from '../../assets/images/settings.svg';
+import logout from '../../assets/images/logout.svg';
+import bmc from '../../assets/images/bmc-button.svg'
 import { Link } from 'react-router-dom';
+
 function Header() {
     return (
         <div className="App">
-          <header className="App-header">
-            <div className="page-logotype">
-                  <Link className="logo-link" to={'/'}>
-                      <img src={logo} alt="Focus Timers Logo"/>
-                      <h1>FOCUS TIMERS</h1>
-                  </Link>
-              </div>
-              <div className="navegation-container">
-                  <nav>
-                      <ul>
-                          <li><Link to={'/pomodoro'}>Pomodoro</Link></li>
-                          <li><Link to={'/login'}>Log In</Link></li>
-                          <li><Link to={'/signin'}>Sign In</Link></li>
-                          <li><Link to={'/sesions'}>Sesions</Link></li>
-                          <li><Link to={'/about'}>About</Link></li>
-                      </ul>
-                  </nav>
-                  <a href="https://www.buymeacoffee.com/algorithmicpaws"><button className="donation-button"></button></a>
-              </div>
-          </header>
+            <header className="App-header">
+                <div className="page-logotype">
+                    <Link className="logo-link" to={'/'}>
+                        <img src={logo} alt="Focus Timers Logo"/>
+                        <h1>FOCUS TIMERS</h1>
+                    </Link>
+                </div>
+                <div className="navegation-container">
+                    <nav>
+                        <ul>
+                            <li><Link to={'/pomodoro'}>Pomodoro</Link></li>
+                            <li><Link to={'/login'}>Log In</Link></li>
+                            <li><Link to={'/signin'}>Sign In</Link></li>
+                            <li><Link to={'/sesions'}>Sessions</Link></li>
+                            <li><Link to={'/about'}>About</Link></li>
+                            <li id='desplegable'><Link to={'/account'}><img src={user} alt='Account' className="user-image"></img></Link>
+                                <ul>
+                                    <li><Link to={'/account'}><img src={user} alt='account'></img>Account</Link></li>
+                                    <li><Link to={'/settings'}><img src={settings} alt='settings'></img>Settings</Link></li>
+                                    <li><Link><img src={logout} alt='logout'></img>Logout</Link></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Link to="https://www.buymeacoffee.com/algorithmicpaws" className='donation-button'><img src={bmc} alt="donation"></img></Link>
+                </div>
+            </header>
         </div>
-      );
+    );
 }
 export default Header;
--- frontend/src/pages/Sesions.jsx
+++ frontend/src/pages/Sesions.jsx
@@ -1,12 +1,75 @@
+import '../styles/sesions.css'
 import React from 'react'
 
 export default function Sesions() {
   return (
     <div>
       <head>
-            <title>Focus Timers | Sesions</title>
-        </head>
-      Sesions
+        <title>Focus Timers | Sessions</title>
+      </head>
+      <main>
+      <div class="history-container">
+            <div class="history-header">
+                <h2>Sessions Histoy</h2>
+                <p>Check your sessions history in different periods of time</p>
+            </div>
+            <div class="history-filters">
+                <div class="filter-group">
+                    <label for="date-filter">Time:</label>
+                    <select id="date-filter">
+                        <option value="week">All</option>
+                        <option value="week">Last week</option>
+                        <option value="month">Last month</option>
+                        <option value="year">Last year</option>
+                    </select>
+                </div>
+                <button class="export-button">Export</button>
+            </div>
+            <div class="session-grid">
+                <div class="session-card">
+                    <div class="session-date">12 Nov 2024 - 15:30</div>
+                    <h3>Study session</h3>
+                    <div class="session-stats">
+                        <div class="stat-item">
+                            <span>Duración Total:</span>
+                            <span>2h 30m</span>
+                        </div>
+                        <div class="stat-item">
+                            <span>Pomodoros Completados:</span>
+                            <span>5</span>
+                        </div>
+                        <div class="stat-item">
+                            <span>Pausas Cortas:</span>
+                            <span>4</span>
+                        </div>
+                        <div class="stat-item">
+                            <span>Pausas Largas:</span>
+                            <span>1</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="session-card">
+                    <div class="session-date">12 Nov 2024 - 15:30</div>
+                    <h3>Study session</h3>
+                    <div class="session-stats">
+                        <div class="stat-item">
+                            <span>Duración Total:</span>
+                            <span>2h 30m</span>
+                        </div>
+                        <div class="stat-item">
+                            <span>Pomodoros Completados:</span>
+                            <span>5</span>
+                        </div>
+                        <div class="stat-item">
+                            <span>Pausas realizadas:</span>
+                            <span>5</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </main>
+
     </div>
   )
 }
